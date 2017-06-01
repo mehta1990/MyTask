c597f82ef20fb615fb7304886a45b5b2c5dadeea
Hitesh Mehta <mehta.hitesh07@hotmail.com>
2017-06-01 13:44:02 +1000

    New built of task

---
+++ NamesSort/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+
+namespace Testnames
+{
+    public class Sorting
+    {
+
+        public static void sortFile(string fname)
+        {
+            //Calling file using string parameter 
+            string name = fname;
+            if (fname.Equals("names.txt"))
+            {
+                ArrayList names = new ArrayList();
+
+                ///Creating a new streamreader object to read the existing text file
+                StreamReader sr;
+                try
+                {
+                    sr = new StreamReader(name);
+
+                    string line;
+                    //Reading a file and putting the content in names ArrayList
+
+                    while (!sr.EndOfStream)
+                    {
+                        line = sr.ReadLine();
+                        names.Add(line);
+                    }
+                    Console.WriteLine("sort-names" + " " + @"names.txt");
+                    // Pause the application so the user can read the information on the screen
+                    Console.ReadLine();
+
+                    //Sorting Array
+                    names.Sort();
+                    string outfile = @"names-sorted.txt";
+                    //Checking whether the file is already in the system
+                    if (File.Exists(outfile))
+                    {
+                        System.IO.File.WriteAllText(outfile, string.Empty); //If there is already some content, clear it
+
+                    }
+                    //Creating new text file
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"names-sorted.txt", false))
+                    {
+                        foreach (string c in names)
+                        {
+                            Console.WriteLine(c);
+                            //Writing to the new text file
+                            file.WriteLine(c);
+                        }
+                    }
+                    //Final output
+                    Console.WriteLine("Finished: created names-sorted.txt");
+                }
+                catch (FileNotFoundException e)
+                {
+                    Console.WriteLine("Sorry! File Not Found");
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Sorry! Please check file name");
+                Console.ReadLine();
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            string filename = "names.txt";
+            Sorting.sortFile(filename);
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string line;
+            int counter = 0;
+            Testnames.Sorting.sortFile(@"names.txt");
+            System.IO.StreamReader file = new System.IO.StreamReader(@"names.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                Console.WriteLine();
+                counter++;
+
+            }
+            file.Close();
+            System.Diagnostics.Debug.WriteLine("This is the output");
+            Console.ReadLine();
+
+        }
+
+    }
+}
